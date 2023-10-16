f5618b28f501c9cc9966abfb54d7537d8501469a
Connor Glynn <66882795+connormaglynn@users.noreply.github.com>
2023-10-16 15:53:27 +0100

    ✨ Add POST `/api/user/add` to allow creation of users (#5)

--- app/clients/db_client.py
+++ app/clients/db_client.py
@@ -23,14 +23,15 @@ class UserModel(db.Model):
 
 class DBClient:
     def __init__(self, app: Flask, session: Session = db.session):
-        app.config[
+        self.app = app
+        self.app.config[
             'SQLALCHEMY_DATABASE_URI'] = f'postgresql://{config.database.user}:{config.database.password}@{config.database.endpoint}/{config.database.name}'
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        db.init_app(app)
+        self.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        db.init_app(self.app)
         self.__session = session
 
         # TODO: Delete this 🔥 these are here for initially setting up the DB - we should migrate to a proper database migration tool like `alembic`.
-        with app.app_context():
+        with self.app.app_context():
             db.create_all()
             # Check if test data already exists before trying to create it. Since all columns should be unique, recreating will fail
             if self.get_user_by_email('test_user_1_email@digital.justice.gov.uk') is None:
@@ -56,3 +57,18 @@ class DBClient:
 
     def get_user_by_github_username(self, github_username: str) -> Type[UserModel] | None:
         return self.__session.query(UserModel).filter_by(github_username=github_username).first()
+
+    def add_users(self, users: list[dict], allowed_users: list[str]):
+        for user in users:
+            if user['github_username'] in allowed_users:
+                self.__add_user(
+                    user['email'], user['slack_username'], user["github_username"])
+            else:
+                self.app.logger.warning(
+                    f'skipping user [{user["github_username"]}] - not in allowed list')
+        self.__session.commit()
+
+    def __add_user(self, email: str, slack_username: str, github_username: str) -> None:
+        self.__session.add(UserModel(email=email,
+                                     slack_username=slack_username,
+                                     github_username=github_username))
--- app/services/user_service.py
+++ app/services/user_service.py
@@ -2,8 +2,15 @@ from app.clients.db_client import DBClient
 
 
 class UserService:
-    def __init__(self, db_client: DBClient):
+    def __init__(self, db_client: DBClient, allowed_users=None):
         self.__db_client = db_client
+        # TODO: Temporary security measure, just to ensure the database doesn't get spammed. We should migrate to a proper authentication flow
+        if allowed_users is None:
+            allowed_users = [
+                'connormaglynn',
+                'PepperMoJ'
+            ]
+        self.__allowed_users = allowed_users
 
     def get_user_by_email(self, email: str):
         return self.__db_client.get_user_by_email(email)
@@ -13,3 +20,6 @@ class UserService:
 
     def get_user_by_github_username(self, github_username: str):
         return self.__db_client.get_user_by_github_username(github_username)
+
+    def add_users(self, users: list[dict]):
+        self.__db_client.add_users(users, self.__allowed_users)
