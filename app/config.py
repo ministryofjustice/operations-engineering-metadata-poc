c8a3ccfaf029e3fee0fa3181d463b8614b53269a
connormaglynn <connor.glynn@digital.justice.gov.uk>
2023-10-16 12:02:48 +0100

    🔧 Fix env var spelling mistake `DATABASE_USER` to `DATABASE_USERNAME`

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ from types import SimpleNamespace
 config = SimpleNamespace(
     database=SimpleNamespace(
         endpoint=os.environ.get('DATABASE_ENDPOINT') or 'localhost:5432',
-        user=os.environ.get('DATABASE_USER') or 'postgres',
+        user=os.environ.get('DATABASE_USERNAME') or 'postgres',
         password=os.environ.get('DATABASE_PASSWORD') or 'postgres',
         name=os.environ.get('DATABASE_NAME') or 'postgres'
     )
