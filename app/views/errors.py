bcf961d33eecf5d37aaf549519cfbcbe6c36b89d
PepperMoJ <sam.pepper@digital.justice.gov.uk>
2023-10-31 11:35:23 +0000

    Search results and error pages added

---
+++ app/views/errors.py
@@ -0,0 +1,60 @@
+from flask import (Blueprint, render_template)
+import logging
+
+errors = Blueprint("errors", __name__)
+logger = logging.getLogger(__name__)
+
+@errors.errorhandler(404)
+def page_not_found(err):
+    """Load 404.html when page not found error occurs
+
+    Args:
+        err : N/A
+
+    Returns:
+        Load the templates/404.html page
+    """
+    logger.debug("A request was made to a page that doesn't exist %s", err)
+    return render_template("404.html"), 404
+
+
+@errors.errorhandler(403)
+def server_forbidden(err):
+    """Load 403.html when server forbids error occurs
+
+    Args:
+        err : N/A
+
+    Returns:
+        Load the templates/403.html page
+    """
+    logger.debug("server_forbidden()")
+    return render_template("403.html"), 403
+
+
+@errors.errorhandler(500)
+def unknown_server_error(err):
+    """Load 500.html when unknown server error occurs
+
+    Args:
+        err : N/A
+
+    Returns:
+        Load the templates/500.html page
+    """
+    logger.error("An unknown server error occurred: %s", err)
+    return render_template("500.html"), 500
+
+
+@errors.errorhandler(504)
+def gateway_timeout(err):
+    """Load 504.html when gateway timeout error occurs
+
+    Args:
+        err : N/A
+
+    Returns:
+        Load the templates/504.html page
+    """
+    logger.error("A gateway timeout error occurred: %s", err)
+    return render_template("504.html"), 504
